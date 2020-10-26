5081a6d17963a05d4fe7e02f8da6848edda79486
amitznati-work-hp <amitznati@gmail.com>
2020-10-27 01:05:45 +0200

    code editor fix

--- template-editor/src/widgets/AddLayoutDialog/widget/components/AddLayoutDialog.customSVGTab.js
+++ template-editor/src/widgets/AddLayoutDialog/widget/components/AddLayoutDialog.customSVGTab.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { Button, Grid } from '@material-ui/core';
 // import { makeStyles } from '@material-ui/core/styles';
-import AceEditor from 'react-ace';
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs';
+import 'prismjs/themes/prism-okaidia.css';
+import { makeStyles } from '@material-ui/core/styles';
 
-import 'ace-builds/src-noconflict/mode-html';
-import 'ace-builds/src-noconflict/theme-monokai';
+const useStyles = makeStyles({
+  textarea: {
+    backgroundColor: '#111',
+    color: '#FFF'
+  }
+});
 
 export default function CustomSVGTab({ onSelect, addText = 'ADD', src = '' }) {
   const [value, setValue] = React.useState(src);
+  const classes = useStyles();
   return (
     <Grid container>
       <Grid item xs={12}>
         <p>SVG</p>
-        <AceEditor
-          mode='html'
-          theme='monokai'
-          onChange={setValue}
-          name='customSVG'
-          defaultValue={value}
-          width='100%'
-          editorProps={{ blockScrolling: true }}
-        />
+        <div className={classes.textarea}>
+          <Editor
+            value={value}
+            onValueChange={setValue}
+            highlight={(code) => highlight(code, languages.svg, 'svg')}
+            padding={10}
+          />
+        </div>
       </Grid>
       <Grid item xs={12}>
         <Button
