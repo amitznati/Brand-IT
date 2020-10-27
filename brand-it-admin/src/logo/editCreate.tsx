d0ac72ce2d466148ef64ade79c38ae4ba6810e28
amitznati-work-hp <amitznati@gmail.com>
2020-10-27 14:31:55 +0200

    shared dependencies

--- brand-it-admin/src/logo/editCreate.tsx
+++ brand-it-admin/src/logo/editCreate.tsx
@@ -10,6 +10,19 @@ import {TemplateEditor, TemplatePreviewForPreview} from 'template-editor';
 import logoImage from './logo.png'
 import 'template-editor/dist/index.css';
 import {makeStyles} from "@material-ui/core/styles";
+import {
+	AppBar, Button, Dialog, DialogActions,
+	DialogContent,
+	DialogTitle,
+	Grid,
+	IconButton,
+	Paper,
+	Tab,
+	Tabs,
+	Toolbar,
+	Typography
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 
 const useStyles = makeStyles(() => ({
 	hiddenTextInput: {
@@ -24,9 +37,40 @@ export const LogoEdit = (props) => (
 	</Edit>
 );
 
+const EditTemplateDialog = ({onClose, open, onSaveTemplate, product}) => {
+	return (
+		<Dialog
+			onClose={onClose}
+			fullScreen
+			open={open}
+			aria-labelledby='simple-dialog-title'
+		>
+			<DialogTitle id='simple-dialog-title'>
+				<AppBar position='static'>
+					<Toolbar>
+						<IconButton
+							color='inherit'
+							onClick={onClose}
+							aria-label='Close'
+						>
+							<CloseIcon />
+						</IconButton>
+						<Typography variant='h6' color='inherit'>
+							Add Layout
+						</Typography>
+					</Toolbar>
+				</AppBar>
+			</DialogTitle>
+			<DialogContent>
+				<TemplateEditor product={product} onSaveTemplate={onSaveTemplate} />
+			</DialogContent>
+		</Dialog>
+	)
+}
 export const LogoCreate = (props) => {
 	const initialTemplate = { templateGradients: [], templateFilters: [], layouts: [] };
 	const [templateString, setTemplateString] = React.useState(JSON.stringify(initialTemplate));
+	const [isEditTemplateOpen, setIsEditTemplateOpen] = React.useState(false);
 	const logo = {
 		name: 'Logo',
 		image: '',
@@ -40,6 +84,7 @@ export const LogoCreate = (props) => {
 	};
 	const onSaveTemplate = template => {
 		setTemplateString(JSON.stringify(template));
+		setIsEditTemplateOpen(false);
 	};
 	const classes = useStyles();
 	return (
@@ -47,8 +92,15 @@ export const LogoCreate = (props) => {
 			<SimpleForm>
 				<TextInput source="name" validate={[required()]}/>
 				<TextInput source="template" value={templateString}/>
-				<TemplateEditor product={logo} onSaveTemplate={onSaveTemplate} />
-				<TemplatePreviewForPreview product={logo} template={JSON.parse(templateString)} />
+				<Button variant="contained" color="primary" onClick={() => setIsEditTemplateOpen(true)}>Edit Template</Button>
+				{isEditTemplateOpen && (
+					<EditTemplateDialog
+						open={true}
+						product={logo}
+						onSaveTemplate={onSaveTemplate}
+						onClose={() => setIsEditTemplateOpen(false)} />
+				)}
+				{!isEditTemplateOpen && <TemplatePreviewForPreview product={logo} template={JSON.parse(templateString)} />}
 			</SimpleForm>
 		</Create>
 	);
